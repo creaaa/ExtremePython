7ab4fe73fa31875abda02f2921fb6d7dc5543423
crea <masataka0504@gmail.com>
2017-09-04 23:52:04 -0700

    4th commit

--- CLI/main.py
+++ CLI/main.py
@@ -1,8 +1,13 @@
-
+"""
 import Special.sunday as sunday
 import Special
 
 print( sunday.filename )
 sunday.hello("Masa")
 
+## __path__ 属性はフォルダにのみ存在する。ファイルに対してはエラーとなる。
 print( Special.__path__ )
+"""
+
+from Farewell2016.Greet import farewell
+farewell()
